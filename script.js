6096a537c440e7f7ae21487fbbf857ae1213443e
Luca <l.tomescu@student.fontys.nl>
2024-11-04 19:48:32 +0100

    +fix

--- script.js
+++ script.js
@@ -698,7 +698,7 @@ function hideLoadingOverlay() {
     }, 300); 
 }
 
-setTimeout(hideLoadingOverlay, 4000);
+setTimeout(hideLoadingOverlay, 8000);
 
 // Function to create the planet 
 function createPlanet() {
